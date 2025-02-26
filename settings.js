6b498cf7922de52f887053b963623bc377839272
adunich59 <aduranicholas59@gmail.com>
2025-02-26 19:22:51 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ global.location = "Port Harcourt, Nigeria" //ur location
 
 //new settings 
 global.botname = 'ELITE-PRO-V2' //ur bot name
-global.ownernumber = '2348109263390' //ur owner number
+global.ownernumber = '2347038821346' //ur owner number
 global.ownername = 'ELITE-PRO-V2' //ur owner name
 global.websitex = "https://youtu.be/@elitepro-empire"
 global.elitepropp = 'https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg' // Change this image link to yours.
@@ -20,7 +20,7 @@ global.packname = "Sticker By"
 global.author = "ELITE-PRO-V2\n\n+2348109263390"
 global.creator = "2348109263390@s.whatsapp.net"
 global.xprefix = '.' //set any prefix 
-global.premium = ["2348109263390"] // Premium User
+global.premium = ["2347038821346"] // Premium User
 //Stop here. Thanks for choosing ELITEPRO.
 
 
